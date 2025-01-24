56443e009e8aad116494d35f6dfb8481af4acbe6
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-24 12:22:22 -0300

    Comunicação Laravel | Vue

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,12 +8,4 @@ Route::get('/', function () {
     return view('welcome');
 })->name('site.home');
 
-Route::get('/teste', function () {
-    return Inertia::render('Teste', [
-        'school' => 'test',
-    ]);
-})->name('site.teste');
-
-Route::get('/ziggy/{param}', function () {
-    echo "Teste";
-})->name('site.ziggy');
+Route::resource('users', UserController::class);
