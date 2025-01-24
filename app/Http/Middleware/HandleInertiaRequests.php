d356cf18fcbe2f45b9da27359892368f42f2dfc1
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-24 16:17:19 -0300

    Inserindo o SSR do Inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware {
     /**
@@ -38,6 +39,10 @@ class HandleInertiaRequests extends Middleware {
                 'name' => 'Matheus Purgato',
                 'email' => 'matheuspurgato@gmail.com',
             ],
+            'ziggy' => fn() => [
+                ...(new Ziggy)->toArray(),
+                'location' => $request->url(),
+            ]
         ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,4 @@ createInertiaApp({
         showSpinner: true,
     },
 })
+
